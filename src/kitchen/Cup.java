af5ee5f5c50722bc3f31c4ce1e143de9b5c59542
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-05-11 17:33:12 +0300

    Update Cup.java

--- src/kitchen/Cup.java
+++ src/kitchen/Cup.java
@@ -7,19 +7,17 @@ public class Cup{
 	private final static Integer MAX_VOLUME = 600;
 	
 	//---------CONSTRUCTORS------------//
-	
-	public Cup(){}
-	
-	public Cup(String name, Integer volume, Integer temperature){
-		liquid = new Liquid(name, temperature);
-		setLiquid(volume);
-	 }
-
-	 //-----------GETTERS-----------//
-	
-	public static Integer getMaxVolume() {
-		return MAX_VOLUME;
-	}
+   	public Cup(){}
+    
+   	public Cup(Liquid liquid, Integer maxVolume){
+    		setLiquid(liquid, maxVolume);
+    	}
+    
+    	public Cup(Liquid liquid){
+    		setLiquid(liquid, MAX_VOLUME);
+    	}
+    
+  	//-----------GETTERS-----------//
 	
 	public Liquid getLiquid() {
 		return liquid;
@@ -27,17 +25,17 @@ public class Cup{
 
 	//-----------SETTERS-----------//
 	public void setLiquid(Integer volume) {
-		if(volume <= MAX_VOLUME)
-			liquid.setVolume(volume);
+		setLiquid(liquid, volume);
+	}
+	public void setLiquid(Liquid newLiquid, Integer maxVolume) {
+		if(newLiquid.getVolume() <= maxVolume)
+			liquid = newLiquid;
 		else
 			System.err.println("WRONG VOLUME!");
 	}
 	
-	//-----------METHODS-----------//
-	public Integer getLiquidVolume() {
-		return liquid.getVolume();
-	}
 	
+	//-----------METHODS-----------//
 	public void print() {
 		System.out.println("//-------Info about cup:--------//");
 		System.out.println("Liquid: " + liquid.getName());
