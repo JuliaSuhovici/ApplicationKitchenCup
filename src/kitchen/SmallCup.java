d43f6c42faf70c1d983772d7b911f5653089c662
JuliaSuhovici <63416383+JuliaSuhovici@users.noreply.github.com>
2020-05-11 17:35:38 +0300

    Update SmallCup.java

--- src/kitchen/SmallCup.java
+++ src/kitchen/SmallCup.java
@@ -1,21 +1,13 @@
 package kitchen;
+import world.Liquid;
 
 public class SmallCup extends Cup {
 	
 	//---------CONSTRUCTORS------------//
-	public SmallCup() {
-		
-	}
+	public SmallCup() {}
 	
-	public SmallCup(String liquidName, Integer liquidVolume){
-		super(liquidName, liquidVolume);
-	}
-	
-	//-----------SETTER-----------//
-	public void setLiquidVolume(Integer liquidVolume) {
-		if(liquidVolume >= 0 && liquidVolume <= 250)
-			this.liquidVolume = liquidVolume;
-		else
-			System.err.println("INCORRECT VOLUME FOR SMALL CUP!");
+	public SmallCup(Liquid liquid){
+		super(liquid, 250);
 	}
 }
+
